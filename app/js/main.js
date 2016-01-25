eac67fd71535ef773b14a705b9225964ccfd099a
Danil Kolpakov <kolpakov.danil@gmail.com>
2016-01-25 15:36:28 +0600

    slider 80%

--- app/js/main.js
+++ app/js/main.js
@@ -3,5 +3,11 @@ window.onload = function() {
 };
 
 $(function () {
-	//=
+	var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 0,
+        loop: true
+    });
 });
--- app/js/vendor/script.js
+++ app/js/vendor/script.js
@@ -1,1 +1,2 @@
 //= ../../../bower_components/jquery/dist/jquery.js
+//= ../../../bower_components/swiper/dist/js/swiper.js
