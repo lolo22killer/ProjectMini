c31991516dd7f06856fab3047f145eb5924b80b1
Danil Kolpakov <kolpakov.danil@gmail.com>
2016-01-28 13:07:12 +0600

    section testimonial 100% + slide

--- app/js/main.js
+++ app/js/main.js
@@ -3,13 +3,25 @@ window.onload = function() {
 };
 
 $(function () {
-	var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
+	var swiper = new Swiper('.s-banner .swiper-container', {
+        pagination: '.s-banner .swiper-pagination',
         slidesPerView: 1,
         paginationClickable: true,
+        grabCursor: true,
         spaceBetween: 0,
+        autoplay: 5500,
+      	autoplayDisableOnInteraction: false,
         loop: true
     });
+
+		var swiper = new Swiper('.s-testimonial .swiper-container', {
+	      pagination: '.s-testimonial .swiper-pagination',
+	      slidesPerView: 1,
+	      paginationClickable: true,
+	      grabCursor: true,
+	      spaceBetween: 0,
+	      autoplay: 5500,
+      	autoplayDisableOnInteraction: false,
+	      loop: true
+	  });
 });
-
-
