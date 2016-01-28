e9f2c5074898c3bf995cb4157950074c1f286b28
Danil Kolpakov <kolpakov.danil@gmail.com>
2016-01-28 18:02:53 +0600

    section portfolio isotope 50%

--- app/js/main.js
+++ app/js/main.js
@@ -24,4 +24,34 @@ $(function () {
       	autoplayDisableOnInteraction: false,
 	      loop: true
 	  });
+
+	  var swiper = new Swiper('.s-portf .swiper-container-h', {
+	      nextButton: '.s-portf .swiper-control__next',
+				prevButton: '.s-portf .swiper-control__prev',
+	      slidesPerView: 1,
+	      loop: true
+	  });
+	  	  var swiper = new Swiper('.s-portf .swiper-container-v', {
+	      pagination: '.s-portf .swiper-pagination-v',
+	      slidesPerView: 1,
+	      paginationClickable: true,
+	      grabCursor: true,
+	      mousewheelControl: true,
+	      direction: 'vertical',
+	      loop: true
+	  });
+
+
+	  // init Isotope
+    var $grid = $('.s-portf__isotope-grid').isotope({
+      itemSelector: '.grid-item'
+    });
+    // filter items on button click
+    $('.s-portf__isotope-filter').on( 'click', 'li', function() {
+      var $this = $(this),
+          filterValue = $this.attr('data-filter');
+
+      $this.addClass('active').siblings('.active').removeClass('active');
+      $grid.isotope({ filter: filterValue });
+    });
 });
--- app/js/vendor/script.js
+++ app/js/vendor/script.js
@@ -1,2 +1,3 @@
 //= ../../../bower_components/jquery/dist/jquery.js
 //= ../../../bower_components/swiper/dist/js/swiper.js
+//= ../../../bower_components/isotope/dist/isotope.pkgd.js'
