972a947f03dd6dac6d755a050f4425be9e93f671
Danil Kolpakov <kolpakov.danil@gmail.com>
2016-01-29 19:58:24 +0600

    html 99% no after before

--- app/js/main.js
+++ app/js/main.js
@@ -1,70 +1,117 @@
-window.onload = function() {
-//=
-};
 
 $(function () {
-	var swiper = new Swiper('.s-banner .swiper-container', {
-        pagination: '.s-banner .swiper-pagination',
-        slidesPerView: 1,
-        paginationClickable: true,
-        grabCursor: true,
-        spaceBetween: 0,
-        autoplay: 5500,
-      	autoplayDisableOnInteraction: false,
-        loop: true
-    });
+
+		/*----------  Subsection comment swiper  ----------*/
+
+		var swiper = new Swiper('.s-banner .swiper-container', {
+				pagination: '.s-banner .swiper-pagination',
+				slidesPerView: 1,
+				paginationClickable: true,
+				grabCursor: true,
+				spaceBetween: 0,
+				autoplay: 5500,
+				autoplayDisableOnInteraction: false,
+				loop: true
+		});
 
 		var swiper = new Swiper('.s-testimonial .swiper-container', {
-	      pagination: '.s-testimonial .swiper-pagination',
-	      slidesPerView: 1,
-	      paginationClickable: true,
-	      grabCursor: true,
-	      spaceBetween: 0,
-	      autoplay: 5500,
-      	autoplayDisableOnInteraction: false,
-	      loop: true
-	  });
+				pagination: '.s-testimonial .swiper-pagination',
+				slidesPerView: 1,
+				paginationClickable: true,
+				grabCursor: true,
+				spaceBetween: 0,
+				autoplay: 5500,
+				autoplayDisableOnInteraction: false,
+				loop: true
+		});
 
-	  var swiper = new Swiper('.s-portf .swiper-container-h', {
-	      nextButton: '.s-portf .swiper-control__next',
+		var swiper = new Swiper('.s-portf .swiper-container-h', {
+				nextButton: '.s-portf .swiper-control__next',
 				prevButton: '.s-portf .swiper-control__prev',
-	      slidesPerView: 1,
-	      loop: true
-	  });
-	  	  var swiper = new Swiper('.s-portf .swiper-container-v', {
-	      pagination: '.s-portf .swiper-pagination-v',
-	      slidesPerView: 1,
-	      paginationClickable: true,
-	      grabCursor: true,
-	      mousewheelControl: true,
-	      direction: 'vertical',
-	      loop: true
-	  });
+				slidesPerView: 1,
+				loop: true
+		});
+
+		var swiper = new Swiper('.s-portf .swiper-container-v', {
+				pagination: '.s-portf .swiper-pagination-v',
+				slidesPerView: 1,
+				paginationClickable: true,
+				grabCursor: true,
+				mousewheelControl: true,
+				direction: 'vertical',
+				loop: true
+		});
 
 
-	  // init Isotope
-    var $grid = $('.s-portf__isotope-grid').isotope({
-      itemSelector: '.grid-item'
+
+		/*----------  Subsection comment isotope  ----------*/
+
+		var $grid = $('.s-portf__isotope-grid').isotope({
+			itemSelector: '.grid-item'
+		});
+
+		$('.s-portf__isotope-filter').on( 'click', 'li', function() {
+			var $this = $(this),
+					filterValue = $this.attr('data-filter');
+			$this.addClass('active').siblings('.active').removeClass('active');
+			$grid.isotope({ filter: filterValue });
+		});
+
+
+
+		/*----------  Subsection comment fancybox  ----------*/
+
+		 $(".fancybox").fancybox({
+			openEffect	: 'none',
+			closeEffect	: 'none'
+		});
+
+
+/*----------  Subsection comment  tooltipster ----------*/
+
+  	$('.s-contact__b-form .form-control').tooltipster({
+        trigger: 'custom',
+        onlyOne: false,
+        position: 'right'
     });
-    // filter items on button click
-    $('.s-portf__isotope-filter').on( 'click', 'li', function() {
-      var $this = $(this),
-          filterValue = $this.attr('data-filter');
-
-      $this.addClass('active').siblings('.active').removeClass('active');
-      $grid.isotope({ filter: filterValue });
-    });
 
 
-   $(".fancybox").fancybox({
-		openEffect	: 'none',
-		closeEffect	: 'none'
-	});
+		/*----------  Subsection comment validate  ----------*/
+
+		$(".s-contact__form").validate({
+			rules: {
+				name: "required",
+				email: {
+					required: true,
+					email: true
+				},
+				message: "required"
+			},
+			messages: {
+				name: "Please specify your name",
+				email: {
+					required: "We need your email address to contact you",
+					email: "Your email address must be in the format of name@domain.com"
+				},
+				message: "Please specify your message"
+			},
+
+	     errorPlacement: function (error, element) {
+            var lastError = $(element).data('lastError'),
+                newError = $(error).text();
+            $(element).data('lastError', newError);
+            if(newError !== '' && newError !== lastError){
+                $(element).tooltipster('content', newError);
+                $(element).tooltipster('show');
+            }
+        },
+        success: function (label, element) {
+            $(element).tooltipster('hide');
+        }
+		});
+
+
 
 
 });
 
-
-// $(window).load(function() {
-//    $('.s-portf__isotope-grid').isotope('reload');
-// })
